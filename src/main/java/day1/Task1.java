3e1b637bb318e80154687e2f8367fd097dcf3325
SergeyShaymardanov18 <sigaev18@yandex.ru>
2020-12-01 00:08:46 +0500

    Add Day1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int j = 10;
+        while (j > 0) {
+            System.out.print("JAVA ");
+            j--;
+        }
     }
 }
