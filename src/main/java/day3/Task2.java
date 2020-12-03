875ec5f146ea29b6c03ec4902a827b86f2cd19af
SergeyShaymardanov18 <sigaev18@yandex.ru>
2020-12-03 13:54:18 +0500

    Add Day3 solution. Tests done

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            double numerator = scanner.nextDouble();
+            double denominator = scanner.nextDouble();
+
+            if (denominator == 0) {
+                break;
+            } else {
+                System.out.println(numerator / denominator);
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            double numerator = scanner.nextDouble();
+            double denominator = scanner.nextDouble();
+
+            if (denominator != 0) {
+                System.out.println(numerator / denominator);
+                continue;
+            }
+            System.out.println("Деление на 0");
+        }
     }
 }
