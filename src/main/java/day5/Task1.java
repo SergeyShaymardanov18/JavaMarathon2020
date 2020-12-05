eab665db37eb787116ad12396e1fb72a19c16063
SergeyShaymardanov18 <sigaev18@yandex.ru>
2020-12-05 13:18:50 +0500

    Add Day5 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,53 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setYearOfRelease(1980);
+        car.setColor("Red");
+        car.setModel("Audi");
+        System.out.println("Year of auto release: " + car.getYearOfRelease());
+        System.out.println("Car color: " + car.getColor());
+        System.out.println("Car model: " + car.getModel());
+    }
+}
+class Car {
+    private int yearOfRelease;
+    private String color;
+    private String model;
 
+    public void setYearOfRelease(int yearOfRelease) {
+        if (yearOfRelease <= 0) {
+            System.out.println("Vy vveli necorrectnoe znachenie");
+        } else {
+            this.yearOfRelease = yearOfRelease;
+        }
+    }
+
+    public int getYearOfRelease () {
+        return yearOfRelease;
+    }
+
+    public void setColor (String color) {
+        if (color.isEmpty()) {
+            System.out.println("Vy vveli pustoe slovo");
+        } else {
+            this.color = color;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String model) {
+        if (model.isEmpty()) {
+            System.out.println("Vy vveli pustoe slovo");
+        } else {
+            this.model = model;
+        }
+    }
+
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,32 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2000,"Green","Ural");
+        System.out.println("Motorbike: " + motorbike.getYearOfRelease() + ", "
+                + motorbike.getColor() + ", " + motorbike.getModel());
+    }
+}
 
+class Motorbike {
+    private int yearOfRelease;
+    private String color;
+    private String model;
+
+    public Motorbike (int yearOfRelease, String color, String model){
+        this.yearOfRelease = yearOfRelease;
+        this.color = color;
+        this.model = model;
+        }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
