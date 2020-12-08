9cdfe5ac1a0e8351bb70a496ea0ddea2fbcc1609
SergeyShaymardanov18 <sigaev18@yandex.ru>
2020-12-08 22:07:37 +0500

    Add Day8 solution. Tests done

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,25 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start1 = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            System.out.print(i + " ");
+        }
+        long finish1 = System.currentTimeMillis();
+        long timeConsumedMillis1 = finish1 - start1;
+        System.out.println();
 
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(i + " ");
+            System.out.print(sb.toString());
+        }
+        long finish2 = System.currentTimeMillis();
+        long timeConsumedMillis2 = finish2 - start2;
+
+        System.out.println();
+        System.out.println("Время конкатенацией " + timeConsumedMillis1);
+        System.out.println("Время StringBuilder " + timeConsumedMillis2);
     }
 }
